905440f437d7627569df3ca1abe0208bed0c094b
Demetrios Michael <demetriosm7@yahoo.com>
2021-07-28 21:28:02 -0400

    Delete explore-your-universe directory

--- explore-your-universe/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const observer = new IntersectionObserver(entries => {
-        document.querySelector(".bar").classList.toggle("bar--bg", entries[0].intersectionRatio < 0.9);
-    }, {
-        threshold: 0.9
-    });
-
-    observer.observe(document.querySelector(".header"));
-});
